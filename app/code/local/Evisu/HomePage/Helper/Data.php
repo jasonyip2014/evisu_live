bc964f1da2267b105373cec6b78939780b765a91
Nikolay Kovalenko <nikolayk@ronis.mail>
2013-09-09 12:38:41 +0300

    Home Page BE

--- app/code/local/Evisu/HomePage/Helper/Data.php
+++ app/code/local/Evisu/HomePage/Helper/Data.php
@@ -6,6 +6,10 @@
  * Time: 9:39 AM
  * To change this template use File | Settings | File Templates.
  */ 
-class Evisu_HomePage_Helper_Data extends Mage_Core_Helper_Abstract {
-
+class Evisu_HomePage_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    public function getVideoUrlByCode($code)
+    {
+        return 'http://player.vimeo.com/video/' . $code . '?title=0&amp;byline=0&amp;portrait=0&amp;color=b400ff&amp;autoplay=1&player_class=fvideo&api=1';
+    }
 }
---
+++ app/code/local/Evisu/HomePage/Model/Config/Source/Bgcolor.php
@@ -0,0 +1,8 @@
+<?php
+class Evisu_HomePage_Model_Config_Source_Bgcolor extends RonisBT_Cms_Model_Entity_Attribute_Source_Options
+{
+    protected $_optionList = array(
+        'light-grey' => 'Light Grey',
+        'dark-grey' => 'Dark Grey',
+    );
+}
