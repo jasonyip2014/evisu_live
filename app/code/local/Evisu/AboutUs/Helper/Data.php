9ec5152392fd5848ffd6cc3a512b6984d545c538
Nikolay Kovalenko <nikolayk@ronis.mail>
2013-10-07 11:30:12 +0300

    About Us Page Module added

---
+++ app/code/local/Evisu/AboutUs/Helper/Data.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: nikolayk
+ * Date: 10/7/13
+ * Time: 10:27 AM
+ * To change this template use File | Settings | File Templates.
+ */ 
+class Evisu_AboutUs_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
---
+++ app/code/local/Evisu/AboutUs/sql/evisu_aboutus_setup/mysql4-install-0.1.0.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: nikolayk
+ * Date: 10/7/13
+ * Time: 10:27 AM
+ * To change this template use File | Settings | File Templates.
+ */ 
+/* @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+
+
+$installer->endSetup();
