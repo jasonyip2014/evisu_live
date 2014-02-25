a536276420abdfd4a6170307796092c827e0522f
Nikolay Kovalenko <nikolayk@ronis.mail>
2014-02-25 18:40:44 +0200

    Collections

---
+++ app/code/local/Evisu/Collection/sql/evisu_collection_setup/mysql4-install-0.1.0.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: nikolayk
+ * Date: 11/6/13
+ * Time: 5:56 PM
+ * To change this template use File | Settings | File Templates.
+ */ 
+/* @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+
+
+$installer->endSetup();
