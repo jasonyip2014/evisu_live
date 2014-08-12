a87ffd73c9d76be0ad2a70dd96a2803145b13e55
leo-leung <leo@cleargo.com>
2014-08-12 18:41:52 +0800

    navigation hide menu item

---
+++ app/code/community/RonisBT/TopMenu/Block/Navigation.php
@@ -0,0 +1,515 @@
+<?php
+
+class RonisBT_TopMenu_Block_Navigation extends Mage_Catalog_Block_Navigation
+{
+    /**
+     * Store categories cache
+     *
+     * @var array
+     */
+    protected $_storeCategories = array();
+
+    const XML_NODE_CATEGORY_DEFAULT_CONFIG = 'global/topmenu/category/default';
+
+    protected $_default_data = null;
+    protected $_menu_renderers = array();
+    protected function _construct()
+    {
+        $this->addData(array(
+            'cache_lifetime'    => 3600,
+            'cache_tags'        => array(Mage_Catalog_Model_Category::CACHE_TAG, Mage_Core_Model_Store_Group::CACHE_TAG),
+        ));
+    }
+
+    /* mobile menu parent categories */
+    protected $brandParentCategory = null;
+    protected $productParentCategory = null;
+
+    /**
+     * Get Key for caching block content
+     *
+     * @return string
+     */
+    public function getCacheKey()
+    {
+        if ($this->hasData('cache_key')) {
+            return $this->getData('cache_key');
+        }
+        /**
+         * don't prevent recalculation by saving generated cache key
+         * because of ability to render single block instance with different data
+         */
+        $key = $this->getCacheKeyInfo();
+        //ksort($key);  // ignore order
+        $key = array_values($key);  // ignore array keys
+        $key = implode('|', $key);
+        $key = sha1($key);
+        return 'CATALOG_NAVIGATION_'.$key;
+    }
+
+    public function getCacheKeyInfo()
+    {
+        $info = array();
+        foreach ($this->_menu_renderers as $key=>$block){
+            $info[$key] = array(get_class($block), $block->getTemplate());
+        }
+        $items = array(
+            'renderers' => serialize($info)
+        );
+        $items = parent::getCacheKeyInfo() + $items;
+        return $items;
+    }
+
+
+    public function getCurrenCategoryKey()
+    {
+        if (!$this->_currentCategoryKey){
+            $category = Mage::registry('current_category');
+            if ($category){
+                $this->_currentCategoryKey = implode('/',array_slice(explode('/',$category->getPath()),0,2));
+            } elseif ($this->getCurrentMenu()){
+                $this->_currentCategoryKey = 'topmenu/'.$this->getCurrentMenu();
+            } else {
+                $this->_currentCategoryKey = '1/'.Mage::app()->getStore()->getRootCategoryId();
+            }
+        }
+        return $this->_currentCategoryKey;
+    }
+
+    protected function getSubmenuHtml($item, $level=1)
+    {
+
+        $type = $item->getType();
+        $block = $item->getBlock();
+        $html = '';
+        if ($type == 'block'){
+            if ($block){
+                $renderer = $this->getMenuRenderer($block);
+                if (!is_null($renderer)){
+                    $renderer->resetSubmenu()
+                        ->setMenuItem($item)
+                        ->setLevel($level);
+                    $html .= $renderer->toHtml();
+                }
+            }
+        } elseif ($type == 'category'){
+            if ($block){
+                $renderer = $this->getMenuRenderer($block);
+                if ($renderer){
+                    $renderer->resetSubmenu()
+                        ->setMenuItem($item)
+                        ->setLevel($level);
+                    $html .= $renderer->toHtml();
+                }
+            } else {
+                return $this->drawCategoryItem($item->getCategory(), $level);
+            }
+        } elseif ($type == 'menu'){
+            return $this->drawSubmenu($item, $level);
+        } else {
+            // unknown item type
+            Mage::log('[topmenu] Unknown top menu item type ' . $type);
+        }
+        return $html;
+    }
+
+    protected function getItemHtml($item, $level=0, $last=false, $customTitle = false)
+    {
+        if($customTitle)
+        {
+            $title = $customTitle;
+        }
+        else
+        {
+            $title = $this->__($item->getTitle());
+        }
+
+        $params = '';
+        $url = $item->getUrl() ? $item->getUrl() : 'javascript:void(0);';
+
+        $position = $this->_getItemPosition($level);
+        $class = ' nav-'.$item->getKey();
+        if ($level == 0){
+            $class .= ' top-level';
+        }
+
+        if ($item->getCategory()){
+            $class .= ' nav-id-'.$item->getCategory()->getId();
+            if ($this->isCategoryActive($item->getCategory()))
+                $class .= ' active';
+        } else {
+            if ($this->getCurrentMenu() == $item->getKey())
+                $class .= ' active';
+            else if ($item->getUrl() && Mage::helper('core/url')->getCurrentUrl() == $item->getUrl())
+                $class .= ' active';
+        }
+        if ($last){
+            $class .= ' last';
+        }
+
+        $submenu = $this->getSubmenuHtml($item, $level);
+        if (!empty($submenu)){
+            $class .= ' parent';
+            if ($level!=0)
+                $params = Mage::helper('topmenu')->getMenuJS();
+        }
+        return sprintf($this->helper('topmenu')->getItemTemplate(), $level, $position, $class, $params, $url, $title, $submenu);
+    }
+
+    public function drawCategoryItem($category, $level=0, $last=false)
+    {
+        $html = '';
+        if (!$category->getIsActive()){
+            return $html;
+        }
+        if (Mage::helper('catalog/category_flat')->isEnabled()) {
+            $children = $category->getChildrenNodes();
+            $childrenCount = count($children);
+        } else {
+            $children = $category->getChildrenCategories();
+            $childrenCount = $children->count();
+        }
+        $hasChildren = $children && $childrenCount;
+        if ($hasChildren){
+            $cnt = 0;
+            $j = 0;
+            foreach ($children as $child){
+                if ($child->getIsActive())
+                    $cnt++;
+            }
+            $htmlChildren = '';
+            foreach ($children as $child) {
+                if ($child->getIsActive()) {
+                    $htmlChildren.= $this->drawItem($child, $level+1, ++$j >= $cnt);
+                }
+            }
+            if (!empty($htmlChildren)){
+                $html.= '<ul class="level' . $level . '">'."\n"
+                        .$htmlChildren
+                        .'</ul>'."\n";
+            }
+        }
+        return $html;
+    }
+
+    public function drawSubmenu($item, $level=0, $last=false)
+    {
+        $html = '';
+        $children = $item->getChildren();
+        $childrenCount = count($children);
+        $hasChildren = $children && $childrenCount;
+        if ($hasChildren){
+            $j = 0;
+            $htmlChildren = '';
+            foreach ($children as $child){
+                $htmlChildren.= $this->drawItem($child, $level+1, ++$j >= $childrenCount);
+            }
+            if (!empty($htmlChildren)){
+                $html.= '<ul class="level' . $level . '">'."\n"
+                        .$htmlChildren
+                        .'</ul>'."\n";
+            }
+        }
+        return $html;
+    }
+
+    public function drawItem($item, $level=0, $last=false, $thumbnail = false)
+    {
+
+        $category = $item->getCategory();
+        //Zend_debug::dump($item['id']);
+        if (preg_match("|evisu/shop-all|i",$category['request_path'])){
+            return false;
+        }
+
+        if (!$this->isActive())
+            return parent::drawItem($item, $level, $last);
+
+        if ($item instanceof RonisBT_TopMenu_Model_Menu_Item)
+            return $this->getItemHtml($item, $level, $last, $thumbnail);
+
+
+        $drawItem = parent::drawItem($item, $level, $last);
+
+
+        return $drawItem;
+    }
+
+    protected function _getDefaultData()
+    {
+        if (is_null($this->_default_data)){
+            $node = Mage::getConfig()->getNode(self::XML_NODE_CATEGORY_DEFAULT_CONFIG);
+            if ($node){
+                $this->_default_data = $node->asArray();
+            } else {
+                $this->_default_data = array();
+            }
+        }
+        return $this->_default_data;
+    }
+
+    public function getInactiveCategoryIds()
+    {
+        if (is_null($this->getData('_inactive_category_ids'))){
+            $inactiveCateroryIds = array();
+            $rootCategoryId = Mage::app()->getStore()->getRootCategoryId();
+            $categoryModel = Mage::getModel('catalog/category');
+            if ($categoryModel->checkId($rootCategoryId)){
+                //
+                // hack to exclude items marked as "include in menu" = no
+                // bug in Magento: by default this attribute is ignored
+                //
+                $inactiveCaterories = $categoryModel->getCategories($rootCategoryId, 0, false, true, false)
+                    ->addAttributeToSelect('*')
+                    ->addAttributeToFilter('level', 2)
+                    ->addAttributeToFilter('is_active', 1)
+                    ->addAttributeToFilter('include_in_menu', 0);
+                foreach ($inactiveCaterories as $category){
+                    $inactiveCateroryIds[] = $category->getId();
+                }
+            }
+            $this->setData('_inactive_category_ids', $inactiveCateroryIds);
+        }
+        return $this->getData('_inactive_category_ids');
+    }
+
+
+    protected function _collectMenuItems()
+    {
+        $top = Mage::getModel('topmenu/menu_item');
+
+        //$inactiveCateroryIds = $this->getInactiveCategoryIds();
+        foreach ($this->getStoreCategories() as $category){
+            //if (!in_array($category->getId(), $inactiveCateroryIds)){
+                $item = Mage::getModel('topmenu/menu_item')
+                    ->assignCategory($category, Mage::helper('topmenu')->getCategoryUrl($category))
+                    ->addData($this->_getDefaultData());
+                $top->addItem($item);
+            //}
+        }
+
+        $keys = array();
+        $items = Mage::helper('topmenu')->getItems();
+        if (!empty($items)){
+            foreach ($items as $item){
+                $item = Mage::getModel('topmenu/menu_item')
+                    ->setData($item);
+                if ($item->getEnabled()){
+                    $keys[$item->getKey()] = $item;
+                }
+            }
+        }
+
+        foreach ($keys as $key=>$item){
+            $type = $item->getType();
+            if ($type == 'category' || $type == 'block'){
+                $top->addItem($item);
+            } else {
+                $parent = $item->getParent();
+                if ($parent){
+                    if (isset($keys[$parent])){
+                        $keys[$parent]->addItem($item);
+                    }
+                } else {
+                    $top->addItem($item);
+                }
+            }
+        }
+        $top->sortItems()->cleanup();
+        return $top;
+    }
+
+    public function getMenuItems()
+    {
+        if (is_null($this->getData('_top_menu'))){
+            $top = $this->_collectMenuItems();
+            $this->setData('_top_menu', $top);
+        }
+        return $this->getData('_top_menu')->getChildren();
+    }
+
+    public function getMenuRenderer($type)
+    {
+        return isset($this->_menu_renderers[$type]) ? $this->_menu_renderers[$type] : null;
+    }
+
+    public function addMenuRenderer($type, $template, $block_type=null)
+    {
+        $block_type = empty($block_type) ? 'topmenu/submenu' : $block_type;
+        $block = $this->getLayout()
+            ->createBlock($block_type, $type, array('template'=>$template));
+        $this->_menu_renderers[$type] = $block;
+        return $this;
+    }
+
+    public function addMenuRendererBlock($type, $block)
+    {
+        $this->_menu_renderers[$type] = $block;
+        return $this;
+    }
+
+    public function isActive()
+    {
+        return Mage::helper('topmenu')->isActive();
+    }
+
+    /**
+     * Retrieve current store categories
+     *
+     * @param   boolean|string $sorted
+     * @param   boolean $asCollection
+     * @return  Varien_Data_Tree_Node_Collection|Mage_Catalog_Model_Resource_Eav_Mysql4_Category_Collection|array
+     */
+    public function getStoreCategories($sorted=false, $asCollection=false, $toLoad=true)
+    {
+        $parent     = Mage::app()->getStore()->getRootCategoryId();
+        $cacheKey   = sprintf('topmenu-%d-%d-%d-%d-'.Mage::app()->getStore()->getId(), $parent, $sorted, $asCollection, $toLoad);
+        Varien_Profiler::start('_storeCategories: '.__METHOD__);
+        if (isset($this->_storeCategories[$cacheKey])/* || ($cache = Mage::app()->loadCache(Mage_Catalog_Model_Category::CACHE_TAG.$cacheKey))*/) {
+            /*if (!isset($this->_storeCategories[$cacheKey]))
+            {
+                $this->_storeCategories[$cacheKey] = unserialize($cache);
+            }*/
+            Varien_Profiler::stop('_storeCategories: '.__METHOD__);
+            return $this->_storeCategories[$cacheKey];
+        }
+
+        /**
+         * Check if parent node of the store still exists
+         */
+        $category = Mage::getModel('catalog/category');
+        /* @var $category Mage_Catalog_Model_Category */
+        if (!$category->checkId($parent)) {
+            if ($asCollection) {
+                return new Varien_Data_Collection();
+            }
+            return array();
+        }
+
+        $recursionLevel  = max(0, (int) Mage::app()->getStore()->getConfig('catalog/navigation/max_depth'));
+        $storeCategories = $category->getCategories($parent, $recursionLevel, $sorted, $asCollection, false);
+
+        $this->_storeCategories[$cacheKey] = $storeCategories;
+        //Mage::app()->saveCache(serialize($storeCategories), Mage_Catalog_Model_Category::CACHE_TAG.$cacheKey, array(Mage_Catalog_Model_Category::CACHE_TAG));
+
+        return $storeCategories;
+    }
+
+    /**
+     * Load block html from cache storage
+     *
+     * @return string | false
+     */
+    protected function _loadCache()
+    {
+        if (is_null($this->getCacheLifetime())) {
+            return false;
+        }
+        $cacheKey = $this->getCacheKey();
+        /** @var $session Mage_Core_Model_Session */
+        $session = Mage::getSingleton('core/session');
+        $cacheData = Mage::app()->loadCache($cacheKey);
+        if ($cacheData) {
+            $cacheData = str_replace(
+                $this->_getSidPlaceholder($cacheKey),
+                $session->getSessionIdQueryParam() . '=' . $session->getEncryptedSessionId(),
+                $cacheData
+            );
+            if ($category = Mage::registry('current_category'))
+            {
+                $currentCategoryKey = explode('/',$category->getPath());
+                if (count($currentCategoryKey)>2)
+                {
+                    $regexp = preg_quote('nav-id-'.$currentCategoryKey[2]);
+                    $cacheData = preg_replace('/(?<=\s|")(' . $regexp . ')(?=\s|")/u', '$1 active', $cacheData);
+                }
+            }
+
+        }
+        return $cacheData;
+    }
+
+    /**
+     * Save block content to cache storage
+     *
+     * @param string $data
+     * @return Mage_Core_Block_Abstract
+     */
+    protected function _saveCache($data)
+    {
+        if (is_null($this->getCacheLifetime())) {
+            return false;
+        }
+        $cacheKey = $this->getCacheKey();
+        /** @var $session Mage_Core_Model_Session */
+        $session = Mage::getSingleton('core/session');
+        $data = str_replace(
+            $session->getSessionIdQueryParam() . '=' . $session->getEncryptedSessionId(),
+            $this->_getSidPlaceholder($cacheKey),
+            $data
+        );
+        $classes = array();
+        $classesCount = preg_match_all('/(?<=\s)class="([^"]*?active[^"]*?)"/u', $data, $classes); //RonisBT: fix regexp
+        for ($i = 0; $i < $classesCount; $i++) {
+            $classAttribute = $classes[0][$i];
+            $classValue = $classes[1][$i];
+            $classInactive = preg_replace('/(\s+active|active\s+|^active$)/', '', $classAttribute); //RonisBT: fix regexp
+            $data = str_replace($classAttribute, $classInactive, $data);
+        }
+        Mage::app()->saveCache($data, $cacheKey, $this->getCacheTags(), $this->getCacheLifetime());
+        return $this;
+    }
+
+    public function getBrandMenuHtml()
+    {
+        $html = '';
+        $brandParentCategory = $this->getBrandParentCategory();
+        $childrenIds = str_replace($this->getProductParentCategory()->getId(), '', $brandParentCategory->getChildren());
+        $subCategories = $brandParentCategory->getCollection()
+            ->addAttributeToSelect(array('name'))
+            ->addIdFilter($childrenIds)
+            ->setOrder('position', 'asc');
+        foreach ($subCategories as $child)
+        {
+            $html .= '<li>';
+            $html .= '<a href="' . $child->getUrl() . '" class="level1">' . $this->escapeHtml($child->getName()) . '</a>';
+            $html .= '</li>';
+        }
+        return $html;
+    }
+
+    public function getProductMenuHtml()
+    {
+        $html = '';
+        $productParentCategory = $this->getProductParentCategory();
+        $subCategories = $productParentCategory->getCollection()
+            ->addAttributeToSelect(array('name'))
+            ->addIdFilter($productParentCategory->getChildren())
+            ->setOrder('position', 'asc');
+        foreach ($subCategories as $child)
+        {
+            $html .= '<li>';
+            $html .= '<a href="' . $child->getUrl() . '" class="level1">' . $this->escapeHtml($child->getName()) . '</a>';
+            $html .= '</li>';
+        }
+        return $html;
+    }
+
+    private function getBrandParentCategory()
+    {
+        if(is_null($this->brandParentCategory))
+        {
+            $this->brandParentCategory = Mage::getModel('catalog/category')->load(Mage::getStoreConfig('catalog/product_detail/brand_parent_category_id'));
+        }
+        return $this->brandParentCategory;
+    }
+
+    private function getProductParentCategory()
+    {
+        if(is_null($this->productParentCategory))
+        {
+            $this->productParentCategory = Mage::getModel('catalog/category')->load(Mage::getStoreConfig('catalog/product_detail/product_parent_category_id'));
+        }
+        return $this->productParentCategory;
+    }
+}
---
+++ app/code/community/RonisBT/TopMenu/Helper/Data.php
@@ -0,0 +1,194 @@
+<?php
+
+class RonisBT_TopMenu_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+    protected $_item_template = '<li class="level%s nav-%s%s"%s><a href="%s"><span>%s</span></a>%s</li>';
+    //level0 nav-1 level-top first last parent active
+
+    protected $_categoryInstance;
+    protected $_layers = array();
+    protected $_layerAttributes = array();
+    protected $_cmsMenuItems = null;
+
+    public function isActive(){
+        return (bool) Mage::getStoreConfigFlag('design/topmenu/enabled');
+    }
+
+    public function getItemTemplate(){
+        $template = (string)Mage::getStoreConfig('design/topmenu/item_template');
+        return $template ? $template : $this->_item_template;
+    }
+
+    public function getMenuJS(){
+        $js = (string)Mage::getStoreConfig('design/topmenu/javascript');
+        return strlen($js) ? ' '.$js : '';
+    }
+
+    public function getItems(){
+        return unserialize((string)Mage::getStoreConfig('design/topmenu/items'));
+    }
+
+    public function getUrlWrapper($url, $params=array()){
+        if (strpos($url, '?') !== false){
+            list($url, $query) = explode('?', $url, 2);
+            if (strpos($query, '#') !== false){
+                list($query, $hash) = explode('#', $query, 2);
+                $params['_fragment'] = $hash;
+            }
+            $params['_query'] = $query;
+            $params['_direct'] = true;
+        }
+        return Mage::getUrl($url, $params);
+    }
+
+    public function getCategoryChildren($category, $sorted=true, $additional=null){
+        $categories = array();
+        if (Mage::helper('catalog/category_flat')->isEnabled()){
+            $children = $category->getChildrenNodes();
+            if (is_null($children)){
+                $children = $category->getChildrenCategories();
+            }
+        } else {
+            $children = $category->getChildren();
+        }
+        foreach ($children as $child){
+            if ($child->getIsActive()){
+
+                    $categories[$child->getName()] = $child;
+
+            }
+        }
+        if ($additional && is_array($additional)){
+            $categories = array_merge($categories, $additional);
+        }
+        if ($sorted){
+            ksort($categories, SORT_LOCALE_STRING);
+        }
+
+        $categories = array_values($categories);
+        return $categories;
+    }
+
+    protected function _getCategoryInstance(){
+        if (is_null($this->_categoryInstance)){
+            $this->_categoryInstance = Mage::getModel('catalog/category');
+        }
+        return $this->_categoryInstance;
+    }
+
+    public function getCategoryUrl($category, $query=null){
+        if (!($category instanceof Mage_Catalog_Model_Category)){
+            $category = $this->_getCategoryInstance()
+                ->setData($category->getData());
+        }
+        if ($category->getAliasUrl()){
+            $url = explode('?', $category->getAliasUrl(), 2);
+            parse_str(isset($url[1]) ? $url[1] : '', $alias_query);
+            if (!is_array($query)){
+                $query = array();
+            }
+            $query = array_merge($alias_query, $query);
+            $url = Mage::getUrl($url[0], array('_query'=>$query));
+        } else {
+            $url = $category->getUrl();
+            if (!empty($query)){
+                $url .= '?'. http_build_query($query);
+            }
+        }
+        return $url;
+    }
+
+    public function getLayer($categoryId){
+        if (!isset($this->_layers[$categoryId])){
+            $category = Mage::getModel('catalog/category')->load($categoryId);
+            $layer = Mage::getModel('catalog/layer')
+                ->setData('current_category', $category)
+                ->apply();
+            $this->_layers[$categoryId] = $layer;
+        }
+        return $this->_layers[$categoryId];
+    }
+
+    public function getLayerAttributes($layer){
+        if (!isset($this->_layerAttributes[$layer->getStateKey()])){
+            $this->_layerAttributes[$layer->getStateKey()] = $layer->getFilterableAttributes();
+        }
+        return $this->_layerAttributes[$layer->getStateKey()];
+    }
+
+    public function getFilterItems($layer, $code, $filterModel='catalog/layer_filter_attribute'){
+        foreach ($this->getLayerAttributes($layer) as $filter){
+            if ($filter->getAttributeCode() == $code){
+                return Mage::getModel($filterModel)
+                    ->setLayer($layer)
+                    ->setAttributeModel($filter)
+                    ->getItems();
+            }
+        }
+        return array();
+    }
+
+    public function getFilterItemUrl($category, $item, $escape=false, $multiselect=false){
+        $suffix = $multiselect ? '[0]' : '';
+        $query = Mage::getModel('core/url')->setQueryParams(array(
+            $item->getFilter()->getRequestVar() . $suffix => $item->getValue(),
+            Mage::getBlockSingleton('page/html_pager')->getPageVarName() => null // exclude current page from urls
+        ));
+        return $this->getCategoryUrl($category) . '?' . $query->getQuery($escape);
+    }
+
+    public function processMenuHtml($block, $name='topMenu', $navName='catalog.topnav'){
+        $html = $block->getChildHtml($name);
+        if ($html){
+            $nav = $block->getChild($name)->getChild($navName);
+            if ($nav){
+                foreach ($nav->getTopMenuCurrentKeys() as $key){
+                    $html = preg_replace('/'.$key.'/', 'active', $html);
+                }
+                $html = preg_replace('/topmenu-key-[^" ]+/', '', $html);
+            }
+        }
+        return $html;
+    }
+
+    /**
+     * Retrieve image URL
+     *
+     * @return string
+     */
+    public function getMenuThumbnailImageUrl($image)
+    {
+        $url = '';
+
+        if ($image) {
+            $url = Mage::getBaseUrl('media').'catalog/category/'.$image;
+        }
+
+        return $url;
+    }
+
+    public function getCmsMenuItems()
+    {
+        if(!$this->_cmsMenuItems)
+        {
+            $this->_cmsMenuItems = Mage::getModel('cmsadvanced/page')
+                ->getCollection()->addAttributeToSelect('*')
+                ->addFieldToFilter('parent_id',Mage::getStoreConfig('design/topmenu/advanced_cms_root_id'))
+                ->addFieldToFilter('is_active',1)
+                ->addFieldToFilter('include_in_menu',1)
+                ->setOrder('position', 'asc');
+        }
+        return $this->_cmsMenuItems;
+    }
+
+    public function getDefaultCategoryImage()
+    {
+        $image = Mage::getModel('catalog/category')
+            ->load(Mage::getStoreConfig('design/topmenu/root_category_id'))
+            ->getMenuThumbnailImage();
+
+        return $image;
+    }
+
+}
